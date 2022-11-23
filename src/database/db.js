425d24c8b6c949ac79e31479d93de9490bc35327
israel henrique castro pontes <israelpontes@MacBook-Pro-de-israel.local>
2022-11-23 12:49:07 -0300

    feat(DB)Conexao com MongoDB

---
+++ src/database/db.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let cachedDb;
+
+const client = new MongoClient(`${process.env.MONGO_URI}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+export default async function connectMongoDB() {
+  if (cachedDb) {
+    return { db: cachedDb, client };
+  }
+
+  await client.connect();
+
+  const db = client.db(process.env.MONGO_DB);
+  cachedDb = db;
+
+  return { db, client };
+}
+
